37c1ced8ecf9e9dd4d892700584e55e2f37b160e
ganyinglong <ganyinglong@126.com>
2018-08-30 11:59:57 +0800

    ddl

--- dml&ddl/ddl_create_table_user_info_01_root.sql
+++ dml&ddl/ddl_create_table_user_info_01_root.sql
@@ -3,22 +3,22 @@
 /*==============================================================*/
 create table nface_manage_db.user_info
 (
-   id_user              varchar(32) not null,
-   user_name            bigint(0) comment ''用户名'',
-   user_password        varchar(30) comment ''用户密码'',
-   department_code      varchar(10) comment ''机构编码'',
-   mobile               varchar(15) comment ''手机号码'',
-   email                varchar(40) comment ''电子邮箱地址'',
-   ch_name              varchar(30),
+   id_user              varchar(36) not null,
+   user_name            varchar(30) comment '用户名',
+   user_password        varchar(30) comment '用户密码',
+   department_code      varchar(10) comment '机构编码',
+   mobile               varchar(15) comment '手机号码',
+   email                varchar(40) comment '电子邮箱地址',
+   ch_name              varchar(30) comment '中文名（昵称）',
    primary key (id_user)
 );
 
-alter table nface_manage_db.user_info comment ''用户表'';
+alter table nface_manage_db.user_info comment '用户表';
 
 /*==============================================================*/
 /* Index: Idx_user                                              */
 /*==============================================================*/
 create unique index Idx_user on nface_manage_db.user_info
 (
-   user_name
+   user_name,mobile
 );
