6fac29b2752609159abf97cf5a8cc896d102c675
ganyinglong <ganyinglong@126.com>
2018-08-30 12:05:44 +0800

    ddl

--- dml&ddl/create_db_and_user_and_grant_01_root.sql
+++ dml&ddl/create_db_and_user_and_grant_01_root.sql
@@ -4,4 +4,5 @@ create user nface_manage_server@'localhost' identified by 'manage_server_123';
 
 GRANT select,insert,update,delete on nface_manage_db.* TO 'nface_manage_server';
 
+
 grant select,insert,update,delete on app_db.* to 'nface_manage_server';
